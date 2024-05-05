0b5ae903e888aa7164646e2e4be7d6ee7bc6b0cb
Greg Lee <lmh4590@naver.com>
2024-05-05 23:32:54 +0900

    tile index grid type layer

--- script_map.js
+++ script_map.js
@@ -1,13 +1,29 @@
-var registry = new naver.maps.MapTypeRegistry(); //지도유형 저장소 사용하기
 
 var mapOptions = {
     center: new naver.maps.LatLng(35.22471386234804, 128.68400799201314),
     zoom: 18,
-    mapTypes: registry,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+        style: naver.maps.MapTypeControlStyle.DROPDOWN
+    },
     mapTypeId: naver.maps.MapTypeId.HYBRID //하이브리드 맵으로 셋팅
 };
 
 
 var map = new naver.maps.Map('map', mapOptions);
 
-map.mapTypes.set(naver.maps.MapTypeId.SATELLITE, naver.maps.NaverStyleMapTypeOptions.getSatelliteMap());
+
+//타일 그리드 지도 유형 넣기
+var tileGridLayer = new naver.maps.Layer('tileGrid', {
+    name: "TileGrid",
+    minZoom: -1,
+    maxZoom: 14,
+    tileSize: new naver.maps.Size(256, 256),
+    getTile: function(x, y, z) {
+        var div = $('<div class="tilegrid">('+ [z, x, y].join(', ')+')</div>');
+
+        return div[0];
+    }
+});
+
+tileGridLayer.setMap(map);
