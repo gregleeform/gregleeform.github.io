fa809f716b409b11db89d1675ae39666c49a3da4
Greg Lee <lmh4590@naver.com>
2024-05-05 23:46:35 +0900

    add osm maptype

--- script_map.js
+++ script_map.js
@@ -6,7 +6,7 @@ var mapOptions = {
     mapTypeControlOptions: {
         style: naver.maps.MapTypeControlStyle.DROPDOWN
     },
-    mapTypeId: naver.maps.MapTypeId.HYBRID //하이브리드 맵으로 셋팅
+    //mapTypeId: naver.maps.MapTypeId.HYBRID //하이브리드 맵으로 셋팅
 };
 
 
@@ -14,17 +14,37 @@ var map = new naver.maps.Map('map', mapOptions);
 
 
 //타일 그리드 지도 유형 넣기
-var div;
-var tileGridLayer = new naver.maps.Layer('tileGrid', {
-    name: "TileGrid",
-    minZoom: -1,
-    maxZoom: 14,
-    tileSize: new naver.maps.Size(256, 256),
-    getTile: function(x, y, z) {
-        var div = $('<div class="tilegrid">('+ [z, x, y].join(', ')+')</div>');
+// var div;
+// var tileGridLayer = new naver.maps.Layer('tileGrid', {
+//     name: "TileGrid",
+//     minZoom: -1,
+//     maxZoom: 14,
+//     tileSize: new naver.maps.Size(256, 256),
+//     getTile: function(x, y, z) {
+//         var div = $('<div class="tilegrid">('+ [z, x, y].join(', ')+')</div>');
 
-        return div[0];
-    }
+//         return div[0];
+//     }
+// });
+
+// tileGridLayer.setMap(map);
+
+
+
+
+var openStreetMapType = new naver.maps.ImageMapType({
+    name: 'OSM',
+    minZoom: 0,
+    maxZoom: 19,
+    tileSize: new naver.maps.Size(256, 256),
+    projection: naver.maps.EPSG3857,
+    repeatX: true,
+    tileSet: "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    provider: [{
+        title: " /OpenStreetMap", // 출처 표기는 이미지 제공처의 정책을 따라야 합니다.
+        link: "http://www.openstreetmap.org/copyright"
+    }]
 });
 
-tileGridLayer.setMap(map);
+map.mapTypes.set('osm', openStreetMapType);
+map.setMapTypeId('osm');
