a95b48bb74748c28411f7a21a701f87b5de9576c
Greg Lee <lmh4590@naver.com>
2024-05-05 23:00:14 +0900

    set mapTypeId to HYBRID

--- script_map.js
+++ script_map.js
@@ -1,6 +1,7 @@
 var mapOptions = {
     center: new naver.maps.LatLng(35.22471386234804, 128.68400799201314),
-    zoom: 18
+    zoom: 18,
+    mapTypeId: naver.maps.MapTypeId.HYBRID
 };
 
 var map = new naver.maps.Map('map', mapOptions);
