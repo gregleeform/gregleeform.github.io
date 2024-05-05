be1b765623297c4c0e0f3800f956f3cfd0136690
Greg Lee <lmh4590@naver.com>
2024-05-05 23:19:27 +0900

    mapTypes registry

--- script_map.js
+++ script_map.js
@@ -1,7 +1,13 @@
+var registry = new naver.maps.MapTypeRegistry(); //지도유형 저장소 사용하기
+
 var mapOptions = {
     center: new naver.maps.LatLng(35.22471386234804, 128.68400799201314),
     zoom: 18,
-    MapTypeId: naver.maps.MapTypeId.HYBRID
+    mapTypes: registry,
+    mapTypeId: naver.maps.MapTypeId.HYBRID //하이브리드 맵으로 셋팅
 };
 
+
 var map = new naver.maps.Map('map', mapOptions);
+
+map.mapTypes.set(naver.maps.MapTypeId.SATELLITE, naver.maps.NaverStyleMapTypeOptions.getSatelliteMap());
